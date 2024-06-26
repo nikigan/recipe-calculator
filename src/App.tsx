9eca3720c3df6e17a54216043cf1c8896da7571b
Никита Ганкин <gankin.n@yandex.ru>
2024-06-26 14:16:21 +0400

    navbar on the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import RecipeForm from '@/components/RecipeForm.tsx'
 
 function App() {
   return (
-    <main className="min-h-screen ">
-      <div className="container py-5">
-        <RecipeForm />
-      </div>
-    </main>
+    <div className="container py-5">
+      <RecipeForm />
+    </div>
   )
 }
 
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
+import { Button } from '@/components/ui/button.tsx'
+import { ChevronLeft, HomeIcon } from 'lucide-react'
+import { paths } from '@/lib/router.tsx'
 
 const RootLayout = () => {
+  const navigate = useNavigate()
+
   return (
-    <main>
+    <main className="overflow-y-auto max-h-[calc(100vh-75px)] h-screen overscroll-none">
       <div className="container py-3">
         <Outlet />
       </div>
+      <nav className="absolute left-0 bottom-0 w-full right-0 px-3 py-3 flex justify-between">
+        <Button size="icon" variant="outline" className="h-12 w-12" onClick={() => navigate(-1)}>
+          <ChevronLeft />
+        </Button>
+        <Button asChild size="icon" variant="outline" className="h-12 w-12">
+          <Link to={paths.recipe.list}>
+            <HomeIcon />
+          </Link>
+        </Button>
+      </nav>
     </main>
   )
 }
